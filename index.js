cb24db4b72f33ce7d24d539ed9a3a6075d5be5d7
jinjf <1585185302@qq.com>
2022-03-20 00:19:29 +0800

    commit sequence

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-04 15:16:59
- * @LastEditTime: 2021-12-04 15:19:16
+ * @LastEditTime: 2022-03-20 00:19:01
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /git-test/index.js
@@ -20,3 +20,7 @@ const splitArray = (arr, n) => {
 };
 
 console.log(splitArray(arr, 3))
+
+const sequence = () => {
+  
+}
