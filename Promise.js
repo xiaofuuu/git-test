635891e33683577d62834659cd4fae9568d86727
jinjf <1585185302@qq.com>
2022-04-14 09:32:56 +0800

    commit

--- Promise.js
+++ Promise.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-20 00:35:07
- * @LastEditTime: 2022-04-14 09:19:28
+ * @LastEditTime: 2022-04-14 09:29:14
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /git-test/Promise.js
