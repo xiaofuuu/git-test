d70598aa39e9fd87a920f6bc2e9196883069a986
jinjf <jinjf@glodon.com>
2022-12-27 10:23:59 +0800

    isPrime

--- object.js
+++ object.js
@@ -20,3 +20,17 @@
 // }
 
 // console.log(customInstance(1, Number))
+
+function isPrime(n) {
+    if (n < 2) {
+        return true
+    }
+    for (let i = 2; i <= n; i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPrime(4))
