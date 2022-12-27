6d1efbb4f6c559e3797bb8da9ae64cbf3d9ac270
jinjf <jinjf@glodon.com>
2022-12-27 10:09:37 +0800

    isPrime

--- object.js
+++ object.js
@@ -1,22 +1,36 @@
-var obj = {
-    name: 'tony',
-    getName: function() {
-        console.log(this.name)
+// var obj = {
+//     name: 'tony',
+//     getName: function() {
+//         console.log(this.name)
+//     }
+// }
+
+// const _getName = obj.getName.bind(obj);
+
+// console.log(_getName())
+
+// function customInstance(left, right) {
+//     let proto = Object.getPrototypeOf(left);
+//     let prototype = right.prototype;
+//     while (true) {
+//         if (!proto) return false
+//         if (proto === prototype) return true
+//         proto = Object.getPrototypeOf(proto)
+//     }
+// }
+
+// console.log(customInstance(1, Number))
+
+function isPrime(n) {
+    if (n < 2) {
+        return true
     }
+    for (let i = 2; i <= n; i++) {
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
 }
 
-const _getName = obj.getName.bind(obj);
-
-console.log(_getName())
-
-function customInstance(left, right) {
-    let proto = Object.getPrototypeOf(left);
-    let prototype = right.prototype;
-    while (true) {
-        if (!proto) return false
-        if (proto === prototype) return true
-        proto = Object.getPrototypeOf(proto)
-    }
-}
-
-console.log(customInstance(1, Number))
+console.log(isPrime(4))
